7d7fb71bcee4d893a1b4fe81b01b7052d81e0124
Ayush Bandil <ayushbandil@finmechanics.com>
2019-12-23 13:40:36 -0800

    FirstCommit

---
+++ src/main/java/lambda/Response.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lambda;
+
+import java.lang.annotation.Native;
+import java.util.List;
+
+/**
+ *
+ * @author wlloyd
+ */
+public class Response extends saaf.Response {
+    
+    //
+    // User Defined Attributes
+    //
+    //
+    // ADD getters and setters for custom attributes here.
+    //
+
+    // Return value
+    private String value;
+    public String getValue()
+    {
+        return value;
+    }
+    public void setValue(String value)
+    {
+        this.value = value;
+    }
+    public  String version;
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public List<String> names;
+    public List<String> getNames()
+    {
+        return this.names;
+    }
+    public void setNames(List<String> names)
+    {
+        this.names = names;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "value=" + this.getValue() + super.toString(); 
+    }
+
+}
